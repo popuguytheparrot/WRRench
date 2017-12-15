9d9da71368eba98891ff5f54a5e2de47f239c31e
Popuguy <d3xrow@gmail.com>
2017-12-15 20:33:07 +0300

    upd component

--- app/components/Body.jsx
+++ app/components/Body.jsx
@@ -5,24 +5,30 @@ import ReduxIcon from 'img/redux.svg'
 import RouterIcon from 'img/react-router.svg'
 
 
-const Body = () => (
-  <section className='body'>
-    <div className='webpack'>
-      <img src={WebpackIcon} alt=""/>
-      <p>Webpack</p>
-    </div>
-    <div className="react">
-      <img src={ReactIcon} alt=""/>
-      <p>React</p>
-    </div>
-    <div className="redux">
-      <img src={ReduxIcon} alt=""/>
-      <p>Redux</p>
-    </div>
-    <div className="router">
-      <img src={RouterIcon} alt=""/>
-      <p>React-Router</p>
-    </div>
+
+const Body = props => (
+  <section className="body">
+    <section className="libs">
+      <div className="webpack">
+        <img src={WebpackIcon} alt="" />
+        <p>Webpack</p>
+      </div>
+      <div className="react">
+        <img src={ReactIcon} alt="" />
+        <p>React</p>
+      </div>
+      <div className="redux">
+        <img src={ReduxIcon} alt="" />
+        <p>Redux</p>
+      </div>
+      <div className="router">
+        <img src={RouterIcon} alt="" />
+        <p>React-Router</p>
+      </div>
+    </section>
+    <section>
+      <div className="git-clone" />
+    </section>
   </section>
 );
 
