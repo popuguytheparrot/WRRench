5563c663a1aa03baffb7b057d8af303731f7dc8f
Popuguy <d3xrow@gmail.com>
2018-02-11 19:49:50 +0300

    Change architecture project. Add router. Upd dependencies.

---
+++ app/components/main/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <div />
+    );
+  }
+}
+
+export default Footer;
---
+++ app/components/routeComponent/SomeRouterComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Body from 'components/main/Body'
+
+const SomeRouterComponent = () => {
+  return (
+    React.cloneElement(
+      <Body />
+    )
+  );
+};
+
+export default SomeRouterComponent;
