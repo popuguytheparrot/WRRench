901c01d3e87bacc9fb3d2ada900562593d4263bf
titov_rv <titov@astralnalog.ru>
2018-04-18 10:47:56 +0300

    add mini css extract plugin without settings

---
+++ webpack/mini.css.extract.js
@@ -0,0 +1,23 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+        ],
+      },
+    ],
+  },
+};
