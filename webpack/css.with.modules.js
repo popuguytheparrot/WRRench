626c9245b6caf6c46acc99f2b236899aa584ba40
Popuguy <d3xrow@gmail.com>
2018-09-27 12:33:19 +0300

    Create css.with.modules.js

---
+++ webpack/css.with.modules.js
@@ -0,0 +1,21 @@
+module.exports = paths => ({
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        include: paths,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: true,
+              localIdentName: '[folder]__[local]--[hash:base64:5]'
+            }
+          }
+        ]
+      }
+    ]
+  }
+});
