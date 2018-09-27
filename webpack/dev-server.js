f26803c6cb1fcbf915df6fc26f645d9ac1a661bf
Popuguy <d3xrow@gmail.com>
2018-09-27 12:28:04 +0300

    Update dev-server.js

--- webpack/dev-server.js
+++ webpack/dev-server.js
@@ -1,9 +1,13 @@
 module.exports = () => ({
   devServer: {
+    contentBase: ['./src/img'],
     historyApiFallback: true,
     hot: true,
     compress: true,
     port: 8080,
+    publicPath: '/',
+    quiet: true,
+    clientLogLevel: 'error',
     headers: {
       'Access-Control-Allow-Origin': '*',
     },
