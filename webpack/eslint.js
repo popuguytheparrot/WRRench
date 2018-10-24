46fafda227158dcca241b41812038ed93e415172
Popuguy <d3xrow@gmail.com>
2018-10-24 17:19:41 +0300

    Create eslint.js

---
+++ webpack/eslint.js
@@ -0,0 +1,20 @@
+module.exports = () => ({
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        enforce: 'pre',
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'eslint-loader',
+            options: {
+              configFile: `./.eslintrc`,
+              emitWarning: true
+            }
+          }
+        ]
+      }
+    ]
+  }
+});
