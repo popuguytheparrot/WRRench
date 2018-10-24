e45cc2a1ef393b5a1d69d1465d0cc03a8d4fe089
Popuguy <d3xrow@gmail.com>
2018-10-24 17:22:16 +0300

    Update optimization.js

--- webpack/optimization.js
+++ webpack/optimization.js
@@ -1,4 +1,7 @@
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCSSsrcPlugin = require('optimize-css-assets-webpack-plugin');
+
+const cssnano = require('cssnano');
 
 module.exports = () => ({
   optimization: {
@@ -8,8 +11,38 @@ module.exports = () => ({
         exclude: /node_modules/,
         cache: true,
         parallel: true,
-        extractComments: true
+        extractComments: true,
+        uglifyOptions: {
+          compress: {
+            unsafe: true,
+            inline: true,
+            passes: 2,
+            keep_fargs: false
+          },
+          output: {
+            beautify: false
+          },
+          mangle: true
+        }
+      }),
+      new OptimizeCSSsrcPlugin({
+        cssProcessor: cssnano,
+        cssProcessorOptions: {
+          discardComments: {
+            removeAll: true
+          },
+          safe: true
+        }
       })
-    ]
+    ],
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendor',
+          chunks: 'all'
+        }
+      }
+    }
   }
 });
